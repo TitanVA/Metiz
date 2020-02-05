8204e308c2762f6994c34477305d11ffa8ce65f5
Viktor Bezai <viktorbezai@gmail.com>
2020-02-05 21:57:08 +0300

    finish alien_invasion. Page 307

--- AlienInvasion/ship.py
+++ AlienInvasion/ship.py
@@ -1,8 +1,10 @@
 import pygame
+from pygame.sprite import Sprite
 
 
-class Ship():
+class Ship(Sprite):
     def __init__(self, ai_settings, screen):
+        super().__init__()
         self.screen = screen
         self.ai_settings = ai_settings
         self.image = pygame.image.load('IMG/ship.png')
@@ -26,3 +28,6 @@ class Ship():
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        self.center = self.screen_rect.centerx
